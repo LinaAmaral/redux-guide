b7ae16e799918f79a1d50c7a08f59dc8c2d13cee
LinaAmaral <lina.amaralcaetano@gmail.com>
2023-08-01 08:53:27 -0300

    lógica para acumular os itens no carrinho

--- src/redux/cart/reducer.js
+++ src/redux/cart/reducer.js
@@ -1,13 +1,31 @@
 import CartActionTypes from "./action-types"
 
 const initialState = {
-    products: []
+    products: [],
+    productPrice:0
 }
 
 const cartReducer = (state = initialState, action) =>{
     switch (action.type){
         case CartActionTypes.ADD_PRODUCT:
-            return {...state, products:[...initialState.products, action.payload]};
+            //verifica se o produto já existe no carrinho
+            const productIsAlreadyInCart = state.products.some(product=>product.id === action.payload.id)
+            //se já existe eu acrescento 1
+            if(productIsAlreadyInCart){
+                return{
+                    ...state,
+                    products:state.products.map((product)=>
+                        product.id === action.payload.id 
+                        ? {...product, quantity: product.quantity + 1}
+                        : product
+                    )
+                }
+            }
+            //senão eu acrescento o produto no carrinho com uma unidade
+            return {
+                ...state,
+                products: [...state.products, {...action.payload, quantity: 1}]
+            }
         default:
             return state;
     }
